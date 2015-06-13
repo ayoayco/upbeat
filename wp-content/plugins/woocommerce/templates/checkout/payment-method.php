86bd9e252c92fb039511279cba75bb470e4cf8c4
ayo <mail@ayoayco.com>
2015-06-14 00:48:24 +0800

    added radio buttons for payment methods; added shipping fee table

--- wp-content/plugins/woocommerce/templates/checkout/payment-method.php
+++ wp-content/plugins/woocommerce/templates/checkout/payment-method.php
@@ -11,40 +11,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<h3>Payment Method</h3>
 <li class="payment_method_<?php echo $gateway->id; ?>">
-	<!--input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+	<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
 
 	<label for="payment_method_<?php echo $gateway->id; ?>">
 		<?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?>
-	</label-->
+	</label>
 	<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
 		<div class="payment_box payment_method_<?php echo $gateway->id; ?>" <?php if ( ! $gateway->chosen ) : ?>style="display:none;"<?php endif; ?>>
 			<?php $gateway->payment_fields(); ?>
 		</div>
 	<?php endif; ?>
 </li>
-
-<table>
-	<tr>
-		<th>Items</th>
-		<th>Courier Service Provider</th>
-		<th>Price</th>
-	</tr>
-	<tr>
-		<td>1 Shirt</td>
-		<td>JRS</td>
-		<td>140 Php</td>
-	</tr>
-	<tr>
-		<td>2-4 Shirts or 1 Jacket</td>
-		<td>LBC (Small Transpack)</td>
-		<td>200 Php</td>
-	</tr>
-	<tr>
-		<td>5-8 Shirts or 2 Jackets</td>
-		<td>LBC (Large Transpack)</td>
-		<td>300 Php</td>
-	</tr>
-</table>
-*Prices are inclusive of the handler's fee
